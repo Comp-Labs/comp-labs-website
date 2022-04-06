9d31851759a9ad6c9dc33fdc43c263fe53e728b9
Comp Labs <complabs28@gmail.com>
2022-04-06 20:42:35 +0530

    Fix 4

--- src/components/HomepageFeatures.jsx
+++ src/components/HomepageFeatures.jsx
@@ -25,7 +25,7 @@ const FeatureList = [
   },
   {
     title: 'App Submission Service for Google Play Developer Console',
-    Svg: require('https://complabs.in/img/google_play.svg').default,
+    Svg: require('../../static/img/google_play.svg').default,
     description: (
       <>
         This is the best solution that helps you publish your <code>AndroidOS</code> and <code>ChromeOS</code> Apps to the <code>Expensive</code> Google Play Console for Free.<br></br><br></br><Link className="button button--primary" to="/solutions/google-play-developers"><Translate>Get Now!</Translate></Link>
