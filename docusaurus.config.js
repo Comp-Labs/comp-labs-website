2927255a65b67d2a8f4365049cc8755b09ab62b6
Rudra Sen <91307601+RudraSen2@users.noreply.github.com>
2022-06-14 18:34:00 +0530

    Fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,18 +61,6 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
           ],
         },
       ],
-      [
-        "ideal-image",
-        /** @type {import('@docusaurus/plugin-ideal-image').PluginOptions} */
-        ({
-          quality: 70,
-          max: 1030,
-          min: 640,
-          steps: 2,
-          // Use false to debug, but it incurs huge perf costs
-          disableInDev: true,
-        }),
-      ],
     ],
 
     presets: [
