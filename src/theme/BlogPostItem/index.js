ae5192f3cd9e1c68b27066eacfca0b43ea3cb88b
Rudra Sen <rudra_sen@outlook.com>
2022-07-24 15:29:27 +0530

    Disqus and Lower Matter Add to All Blog Posts

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import BlogPostItem from '@theme-original/BlogPostItem';
+import React from "react";
+import { DiscussionEmbed } from "disqus-react";
+import { useBlogPost } from "@docusaurus/theme-common/internal";
+import BlogPostItem from "@theme-original/BlogPostItem";
 
 export default function BlogPostItemWrapper(props) {
+  const { metadata } = useBlogPost();
+  const { frontMatter, slug, title } = metadata;
+  const { comments = true } = frontMatter;
   return (
     <>
       <BlogPostItem {...props} />
+      <hr></hr>
+      <strong>Subscribe to our Channel:</strong>
+      <iframe
+          src="https://complabs.in/html/subscribe.html"
+          title="Subscribe Comp Labs"
+          height="100"
+        ></iframe>
+      <h2>Comments</h2>
+      {comments && (
+        <DiscussionEmbed
+          shortname="comp-labs"
+          config={{
+            url: slug,
+            identifier: slug,
+            title,
+            language: "en_US",
+          }}
+        />
+      )}
     </>
   );
 }
