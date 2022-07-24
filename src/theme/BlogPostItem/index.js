828f1b11f68e4716c2f484e74de29192c045df6d
Rudra Sen <rudra_sen@outlook.com>
2022-07-24 09:40:18 +0000

    Swizzle Complete

---
+++ src/theme/BlogPostItem/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BlogPostItem from '@theme-original/BlogPostItem';
+
+export default function BlogPostItemWrapper(props) {
+  return (
+    <>
+      <BlogPostItem {...props} />
+    </>
+  );
+}
