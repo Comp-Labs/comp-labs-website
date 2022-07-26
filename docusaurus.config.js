99f744be1f8e03a58bce78110b5e8c4b7cdb6271
Rudra Sen <rudra_sen@outlook.com>
2022-07-26 19:53:36 +0530

    Fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,6 +23,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
     scripts: [
       "https://kit.fontawesome.com/307bcbc229.js",
       "https://clfiles.herokuapp.com/js/scrolltotop.js",
+      "https://clfiles.herokuapp.com/js/sharebtn.js",
       // 'https://complabs.in/herojs.js',
       // Object format.
       {
