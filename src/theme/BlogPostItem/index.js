54a97b772df73c8f2061aae7dc4fa84963943d15
Rudra Sen <rudra_sen@outlook.com>
2022-07-26 19:50:04 +0530

    Add Share Button on All Blog Posts

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -13,10 +13,10 @@ export default function BlogPostItemWrapper(props) {
       <hr></hr>
       <strong>Subscribe to our Channel:</strong>
       <iframe
-          src="https://clfiles.herokuapp.com/html/subscribe.html"
-          title="Subscribe Comp Labs"
-          height="100"
-        ></iframe>
+        src="https://clfiles.herokuapp.com/html/subscribe.html"
+        title="Subscribe Comp Labs"
+        height="100"
+      ></iframe>
       <h2>Comments</h2>
       {comments && (
         <DiscussionEmbed
@@ -29,6 +29,14 @@ export default function BlogPostItemWrapper(props) {
           }}
         />
       )}
+      <div class="grid place-items-center min-h-screen">
+        <button
+          id="share-button"
+          class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
+        >
+          <i class="fa-solid fa-share-nodes"></i> Share
+        </button>
+      </div>
     </>
   );
 }
