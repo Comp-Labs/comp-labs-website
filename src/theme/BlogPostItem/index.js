5a1f8dc6bbf5537cdcc778967ebac626c4210abf
Rudra Sen <rudra_sen@outlook.com>
2022-07-27 20:52:46 +0530

    Share Button Fix

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { DiscussionEmbed } from "disqus-react";
 import { useBlogPost } from "@docusaurus/theme-common/internal";
 import BlogPostItem from "@theme-original/BlogPostItem";
-// import styles from "./style.css";
 
 export default function BlogPostItemWrapper(props) {
   const { metadata } = useBlogPost();
@@ -30,15 +29,12 @@ export default function BlogPostItemWrapper(props) {
           }}
         />
       )}
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css"></link>
-      <div class="grid place-items-center min-h-screen">
-        <button
-          id="share-button"
-          class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
-        >
-          <i class="fa-solid fa-share-nodes"></i> Share
-        </button>
-      </div>
+      <iframe
+        src="https://clfiles.herokuapp.com/html/sharebtn/index.html"
+        title="Share Button"
+        height="100"
+        width="200"
+      ></iframe>
     </>
   );
 }
