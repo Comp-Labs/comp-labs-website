4c582be0682933b91064069ab05a8da4b5dac121
Rudra Sen <rudra_sen@outlook.com>
2022-07-27 19:29:18 +0530

    Update Check

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { DiscussionEmbed } from "disqus-react";
 import { useBlogPost } from "@docusaurus/theme-common/internal";
 import BlogPostItem from "@theme-original/BlogPostItem";
-import styles from "https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css";
+// import styles from "./style.css";
 
 export default function BlogPostItemWrapper(props) {
   const { metadata } = useBlogPost();
