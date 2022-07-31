fb94f6dc12a578be8693a00c7efed7b78ffd4d8d
Rudra Sen <rudra_sen@outlook.com>
2022-07-31 19:40:52 +0530

    Change to Giscus

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -10,31 +10,6 @@ export default function BlogPostItemWrapper(props) {
   return (
     <>
       <BlogPostItem {...props} />
-      <hr></hr>
-      <strong>Subscribe to our Channel:</strong>
-      <iframe
-        src="https://clfiles.herokuapp.com/html/subscribe.html"
-        title="Subscribe Comp Labs"
-        height="100"
-      ></iframe>
-      <h2>Comments</h2>
-      {comments && (
-        <DiscussionEmbed
-          shortname="comp-labs"
-          config={{
-            url: slug,
-            identifier: slug,
-            title,
-            language: "en_US",
-          }}
-        />
-      )}
-      <iframe
-        src="https://clfiles.herokuapp.com/html/sharebtn/index.html"
-        title="Share Button"
-        height="100"
-        width="200"
-      ></iframe>
     </>
   );
 }
