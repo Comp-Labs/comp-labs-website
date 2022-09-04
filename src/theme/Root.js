960ff7ae3d966280877065e3d7805d1f68cce157
Rudra Sen <rudra_sen@outlook.com>
2022-09-04 15:03:28 +0000

    fix: Netlify Build Error

--- src/theme/Root.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Giscus from '@giscus/react';
-import Link from '@docusaurus/Link';
-
-// Default implementation, that you can customize
-export default function Root({children}) {
-  return <>{children}</>;
-}
