687d476f78448fb310c36d99bcdd512e12f93650
Rudra Sen <rudra_sen@outlook.com>
2022-09-25 06:44:57 +0000

    Fix 3
    Signed-off-by: Rudra Sen <rudra_sen@outlook.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,32 +8,6 @@ import HomepageFeatures from "../components/HomepageFeatures";
 import Translate, { translate } from "@docusaurus/Translate";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 var ReactRotatingText = require("react-rotating-text");
-import BlogPostItem from "@theme/BlogPostItem";
-import { Content } from "@theme/BlogPostPage";
-// import { BlogPostProvider } from "@docusaurus/theme-common/internal";
-
-interface Props {
-  readonly recentPosts: readonly { readonly content: Content }[];
-}
-
-// function RecentBlogPosts({ recentPosts }: Props): JSX.Element {
-//   <div className="container margin-top--xl">
-//     <div className="row">
-//       <div className="col col--9 col--offset-1">
-//         {recentPosts.map(({ content: BlogPostContent }) => (
-//           <BlogPostProvider
-//             key={BlogPostContent.metadata.permalink}
-//             content={BlogPostContent}
-//           >
-//             <BlogPostItem>
-//               <BlogPostContent />
-//             </BlogPostItem>
-//           </BlogPostProvider>
-//         ))}
-//       </div>
-//     </div>
-//   </div>
-// }
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
--- src/theme/Footer/Layout/index.js
+++ src/theme/Footer/Layout/index.js
@@ -15,10 +15,6 @@ export default function FooterLayout({ style, links, logo, copyright }) {
           </div>
         )}
       </div>
-      {/* <div id="preloader">
-        <div id="status">&nbsp;</div>
-      </div> */}
-      {/* <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/Comp-Labs/cdn/js/loader.js"></script> */}
       <script type="text/javascript" src="https://cdn.ywxi.net/js/1.js" async></script>
       <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-629de96c243ef6ee"></script>
     </footer>
