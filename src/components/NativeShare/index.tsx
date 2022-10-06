0d0cb5f4fe2f51cddac6586ffd41dee0e63b5e29
Rudra Sen <rudra_sen@outlook.com>
2022-10-06 13:15:20 +0000

    Fix
    Signed-off-by: Rudra Sen <rudra_sen@outlook.com>

--- src/components/NativeShare/index.tsx
+++ src/components/NativeShare/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
 import { RWebShare } from "react-web-share";
 import { CssVarsProvider } from "@mui/joy/styles";
 import Button from "@mui/joy/Button";
@@ -6,7 +6,6 @@ import BrowserOnly from '@docusaurus/BrowserOnly';
 
 const NativeShare = () => {
     return (
-        <>
         <BrowserOnly>
             <CssVarsProvider>
                 <RWebShare
@@ -20,8 +19,7 @@ const NativeShare = () => {
                     <Button onClick={window.location.href} color="primary" size="md" variant="soft"><i className="fa-solid fa-share-nodes"></i> Share</Button>
                 </RWebShare>
             </CssVarsProvider>
-            </BrowserOnly>
-        </>
+        </BrowserOnly>
     );
 };
 
