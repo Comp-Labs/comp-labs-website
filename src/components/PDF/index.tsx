9beddb9bd45b5ba3236d97bb1f61d77707ff64cb
Rudra Sen <rudra_sen@outlook.com>
2022-11-04 13:35:04 +0530

    Fix

--- src/components/PDF/index.tsx
+++ src/components/PDF/index.tsx
@@ -1,11 +1,8 @@
 import React, { useEffect } from 'react';
 import PictureAsPdfRoundedIcon from '@mui/icons-material/PictureAsPdfRounded';
 
-useEffect(() => {
-    const currentURL = window.location.href;
-})
-
 export default function PDF() {
+    const currentURL = window.location.href;
     <Button
         component="a"
         href={"https://api.microlink.io?url=" + currentURL + "&format=Tabloid&pdf=true&meta=false&embed=pdf.url"}
