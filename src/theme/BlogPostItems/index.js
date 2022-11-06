3451b5039b23dc3f05b1f871104cd34a8b5024c9
Rudra Sen <rudra_sen@outlook.com>
2022-11-06 19:07:54 +0530

    feat: Test/Experiment BlogPostItems

--- src/theme/BlogPostItems/index.js
+++ src/theme/BlogPostItems/index.js
@@ -7,6 +7,7 @@ export default function BlogPostItems({
 }) {
   return (
     <>
+    <img src="https://cdn.jsdelivr.net/gh/Comp-Labs/cdn/img/defaultbg.jpg"></img>
       {items.map(({content: BlogPostContent}) => (
         <BlogPostProvider
           key={BlogPostContent.metadata.permalink}
